cb21ea5ccee1eb3104af0c41e2b8118778ebc5bc
Muzzamil Ahmed Khan <muzzamil.khan@synergates.com>
2025-10-15 12:43:02 +0500

    added models and import laracademy/generators

---
+++ app/Models/Settings/Settings.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Models\Settings;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int    $created_at
+ * @property int    $updated_at
+ * @property string $description
+ * @property string $key
+ * @property string $value
+ */
+class Settings extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'settings';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'created_at',
+        'description',
+        'key',
+        'updated_at',
+        'value'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at' => 'timestamp',
+        'description' => 'string',
+        'key' => 'string',
+        'updated_at' => 'timestamp',
+        'value' => 'string'
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at'
+    ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var boolean
+     */
+    public $timestamps = false;
+
+    // Scopes...
+
+    // Functions ...
+
+    // Relations ...
+}
